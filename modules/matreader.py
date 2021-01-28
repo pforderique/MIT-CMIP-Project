5fa4936d5d09809e105c6de18c6fc05b29bf967d
pforderique <porderiq@mit.edu>
2021-01-28 18:02:36 -0500

    deconstructing mat lab file structure

--- modules/matreader.py
+++ modules/matreader.py
@@ -6,5 +6,32 @@ Piero Orderique
 
 Learning how to use scipy.io lib to open and read mat files
 '''
-import scipy.io
-help(scipy.io)
+from os import scandir
+from scipy.io import loadmat
+
+def create_files_list(directory):
+    files_list = []
+    with scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_file():
+                files_list.append(entry.name)
+    return files_list
+
+mat_files_directory = "mat_files/"
+# mat_files = create_files_list(mat_files_directory)
+
+##############################################################################
+# testing 
+##############################################################################
+filename = r"mat_files\CMIP5_historical_tasmax.mat"
+file = loadmat(filename)
+
+results = file["results"]
+results_struct = {
+    "rra": results["Era"][0][0][0],
+    "variable": results["Variable"][0][0][0],
+    "big_data": results["GCM"][0][0][0][0][13][0][0][0],
+    "temps_by_date": results["GCM"][0][0][0][0][13][0][0][1]
+}
+
+print(results["GCM"][0][0][0][0][13][0][0][1])
