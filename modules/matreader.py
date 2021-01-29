fe5dd3447a8cb3e46b16168a1b199a9cfa29402e
pforderique <porderiq@mit.edu>
2021-01-28 20:47:38 -0500

    created file reader class and subclass

--- modules/matreader.py
+++ modules/matreader.py
@@ -9,6 +9,22 @@ Learning how to use scipy.io lib to open and read mat files
 from os import scandir
 from scipy.io import loadmat
 
+class FileReader():
+    def __init__(self, file_name) -> None:
+        self.file_name = file_name
+        self.extension = file_name[file_name.find("."):] # caution! error if '.' in name
+
+    def get_file_name(self):
+        return self.file_name
+
+    def get_extension(self):
+        return self.extension
+
+class MatFileReader(FileReader):
+    def __init__(self, mat_file_name) -> None:
+        super().__init__(mat_file_name)
+        pass
+
 def create_files_list(directory):
     files_list = []
     with scandir(directory) as entries:
@@ -34,13 +50,16 @@ def extract_info(filename):
 
     return ''.join(era), ''.join(data_type)
 
-######################## DRIVER CODE ################################
+################################ DRIVER CODE ################################
 mat_files_directory = "mat_files/"
 mat_file_names = create_files_list(mat_files_directory) # Comment out for testing rn
 mat_file_name = r"mat_files\CMIP5_historical_tasmax.mat"
 
 # use this to create multiple file reader objects
-for file_name in mat_file_names: 
-    print(file_name) # for degugging
-    era, data_type = extract_info(file_name)
-    print(era, data_type)
+# for file_name in mat_file_names: 
+#     print(file_name) # for degugging
+#     era, data_type = extract_info(file_name)
+#     print(era, data_type)
+
+f1 = MatFileReader(mat_file_name)
+print(f1.get_extension())
