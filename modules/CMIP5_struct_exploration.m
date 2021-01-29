ee09119cf41a97eb1ca89bbb99a94ad02c11f5f9
pforderique <porderiq@mit.edu>
2021-01-28 23:44:59 -0500

    added exploration code

---
+++ modules/CMIP5_struct_exploration.m
@@ -0,0 +1,12 @@
+% open mat file in program to get "results" data
+
+% take a look at gcm's fields
+gcm = results.gcm;
+disp(gcm);
+
+% get the annual maxes for each decade
+annuals = gcm.AnnualMax;
+disp(annuals)
+
+% get data point by data point - 1.85000 is 1850!
+annuals(1)
