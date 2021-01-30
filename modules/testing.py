bc98f4b8f0b3ff317b7828ec79322f1170e86fe1
pforderique <porderiq@mit.edu>
2021-01-29 20:44:37 -0500

    set up metplottertemp class for plotting temp data

--- modules/testing.py
+++ modules/testing.py
@@ -20,7 +20,7 @@ def create_files_list(directory):
     return files_list
 
 mat_file_names = create_files_list(mat_files_directory) 
-mat_file_name = r"CMIP5_historical_tasmax.mat"
+mat_file_name = r"CMIP5_rcp45_tasmax.mat"
 
 ################################ DRIVER CODE ################################
 mfr = MatFileReader(mat_file_name)
@@ -42,4 +42,47 @@ def plot_monthly():
     plt.plot(decades, temps, 'bo')
     plt.show()
 
-plot_monthly()
+def sequence_decades(monthlymax):
+    ''' returns a list of sequences for each decade that contains data for boxplot '''
+    decades = monthlymax[:,0] # 12 data points each decade
+    temps = monthlymax[:,2]
+
+    sequences = []
+    for dec in range(0, len(monthlymax), 12): # every 12 months
+        sequences.append(temps[dec:dec+12])
+
+    return sequences
+
+class MatPlotterTemp():
+    def __init__(self, mfr:MatFileReader) -> None:
+        # set up plotter variables 
+        self.mfr = mfr
+        self.plottables = mfr.GCM_FIELDS[mfr.variable]
+        self.annualmax = self.plottables["AnnualMax"]
+        self.monthlymax = self.plottables["MonthlyMax"]
+        self.decades_list = annualmax[:,0]
+
+        # plt features
+        self.BAR_WIDTH = 5
+        plt.figure(figsize=(10,4))
+
+    def plot_annual(self):
+        temps = annualmax[:,1]
+
+        plt.title("Annual Max Temperature")
+        plt.xlabel("Year")
+        plt.ylabel("Temp " + f'({self.plottables["Unit"]})')
+        plt.bar(self.decades_list, temps, width=self.BAR_WIDTH)
+        plt.show()
+
+# plot_monthly()
+decade_sequences = sequence_decades(monthlymax)
+decades = annualmax[:,0]
+
+# plt.figure(figsize=(10,4))
+# plt.ylim(0, 55)
+# plt.boxplot(decade_sequences, positions=[int(year) for year in decades], widths=5)
+# plt.show()
+
+plotter = MatPlotterTemp(mfr)
+plotter.plot_annual()
